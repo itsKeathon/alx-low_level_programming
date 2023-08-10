77b4403084d94746c844cb18f33ceb810aeea201
itsKeathon <kkeathonlincoln2803@gmail.com>
2023-08-10 08:15:35 -0700

    Update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#Include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ printf("with proper grammar, but the outcome is a piece of art,\n");return(0);}
