88448bfc728e6117fef4f06473f9e8998f52e37e
itsKeathon <kkeathonlincoln2803@gmail.com>
2023-08-15 15:25:19 -0700

    updated nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -19,10 +19,10 @@ int print_sign(int n)
 		_putchar(45);
 		return (-1);
 	}
-        else
+       	else
 	{
 	        _putchar(48);
 	        return (0);
 	}
-        _putchar('\n');
+       	_putchar('\n');
 }	
